f7754d4cbf7dec5591772dee12b8609db88311dc
ZIX_HUB <sxazyhub@gmail.com>
2026-07-19 16:10:13 -0700

    Update Z3R.lua

--- Z3R.lua
+++ Z3R.lua
@@ -1,7 +1,7 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vhhvwvvchvvydvbff-wq/Script-/refs/heads/main/To%C3%B1itax%20librari", true))()
 
-local window = library:AddWindow("Sync X V1 | Public", {
-    main_color = Color3.fromRGB(18, 18, 18),
+local window = library:AddWindow("𝑆𝐸𝑅𝐴𝑃𝐻 𝐻𝑈𝐵 | Public", {
+    main_color = Color3.fromRGB(0, 0, 0),
     min_size = Vector2.new(700, 700),
     can_resize = true,
 })
